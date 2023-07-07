c710004a940c8a9c5e5bcc7b1bce6aa2af779773
Santiago <sncarp2003@gmail.com>
2023-07-07 01:47:15 -0300

    Ejerciciomod2

--- index.py
+++ index.py
@@ -1,14 +1,24 @@
-import operaciones
+import time
 
 def main():
-    sum = operaciones.suma(4, 16)
-    res= operaciones.resta(10, 5)
-    mult = operaciones.multiplicacion(5, 5)
-    div = operaciones.division(25, 5)
-    print(sum)
-    print(res)
-    print(mult)
-    print(div)
+    import time
+
+def calcular_tiempo_restante():
+    hora_actual = time.localtime().tm_hour
+    minutos_actual = time.localtime().tm_min
+    hora_limite = 19
+
+    if hora_actual >= hora_limite:
+        print("¡Es hora de ir a casa!")
+    else:
+        minutos_restantes = (hora_limite - hora_actual - 1) * 60 + (60 - minutos_actual)
+        horas_restantes = minutos_restantes // 60
+        minutos_restantes %= 60
+
+        print("Aún queda tiempo de trabajo. Tiempo restante: {} horas y {} minutos.".format(horas_restantes, minutos_restantes))
+
+calcular_tiempo_restante()
+
 
 if __name__ == '__main__':
     main() 
